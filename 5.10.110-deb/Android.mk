a7f19efcdcdf2290e9b7f6f417448d3fa03ea428
DHDAXCW <lasstp5011@gmail.com>
2023-03-16 14:37:10 +0000

    f

--- 5.10.110-deb/Android.mk
+++
@@ -1,6 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE := diaggrabpro
-LOCAL_SRC_FILES := diaggrabpro.c ftp_client.c mdm.c
-LOCAL_MODULE_TAGS := debug
-include $(BUILD_EXECUTABLE) 
--- 5.10.110-deb/ftp_client.h
+++
@@ -1,1 +0,0 @@
-int ftp_main(int argc, char *argv[]);
